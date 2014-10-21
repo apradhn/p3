eac24664edd1126b96b56c3204010b59bb8cb257
Abhijit Pradhan (macbook) <abhijit.pradhan6@gmail.com>
2014-10-20 21:21:56 -0500

    Added logic for font-size, font-face and columns inputs to Lorem Ipsum generator

--- app/routes.php
+++ app/routes.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Function for generating random Lorem Ipsum text
+ * Returns random Lorem Ipsum text
  * 
  * @param string | int $p 
  */
@@ -22,23 +22,75 @@ function generate_loremipsum($p) {
 }
 
 /**
- * Function for generating random user data
+ * Returns a font-size
+ * 
+ * @param string $fs
+ */ 
+function font_size($fs) {
+	if ($fs == 8) {
+		return "font-size-8";
+	}
+	elseif ($fs == 10) {
+		return "font-size-10";
+	}
+	elseif ($fs == 12) {
+		return "font-size-12";
+	} 
+	elseif ($fs == 14) {
+		return "font-size-14";
+	}
+	elseif ($fs == 16) {
+		return "font-size-16";
+	}
+}
+
+/**
+ * Returns a column #
+ * 
+ * c 
+ */
+function columns($c) {
+	if ($c == 2) {
+		return "two-columns";
+	}
+	elseif ($c == 3) {
+		return "three-columns";
+	}
+}
+
+/** 
+ * Returns a font face
+ * 
+ * @param string $ff
+ */
+function font_face($ff) {
+	if ($ff == 'roboto') {
+		return 'roboto';
+	}
+	elseif ($ff == 'helvetica') {
+		return 'helvetica';
+	}
+	elseif ($ff == 'georgia') {
+		return 'georgia';
+	}
+	elseif ($ff == 'garamond') {
+		return 'garamond';
+	}
+}
+/**
+ * Returns random user data
  *
  * @param string | int $u
  */
 function generate_users($u) {
 	// Creates a faker object 
 	$faker = Faker\Factory::create();
-
 	$users="";
-
 	$birthdate = Input::get('birthdate');
-
 	$profile = Input::get('profile');
 
 	if (((is_numeric($u)) or ($u == NULL)) and ($u < 100))  {
 		// executes of user input is numeric or null 
-
 		if (($birthdate == 0) and ($profile == 0)) {
 			// Executes if neither birthdate nor profile are selected
 			for ($i=0; $i<$u; $i++) {
@@ -71,7 +123,6 @@ function generate_users($u) {
 		// executes if user input is a non-numeric string
 		return "<div class='error'>invalid input</div>";
 	}
-
  } 
 
 /*
@@ -87,24 +138,29 @@ function generate_users($u) {
 
 Route::get('/', function()
 {
-
 	return View::make('index');
-
 });
 
 
 Route::get('/lorem-ipsum', function() 
 {
-
-	// retrieve number of paragraphs
-	$p_count = Input::get('paragraphs');
-
 	// Generate random lorem ipsum text 
-	$paragraphs = generate_loremipsum($p_count);
+	$paragraphs = generate_loremipsum(Input::get('paragraphs'));
+
+	// Changes the font size of the paragraphs
+	$fontSize = font_size(Input::get('font-size'));
+
+	// Changes the font face of the paragarphs
+	$fontFace = font_face(Input::get('font-face'));
+
+	// Changes the number of columns of paragraphs
+	$columns = columns(Input::get('columns'));
 	
 	return View::make('lorem-ipsum')
-		->with('output', $paragraphs);
-
+		->with('paragraphs', $paragraphs)
+		->with('fontSize', $fontSize)
+		->with('fontFace', $fontFace)
+		->with('columns', $columns);
 });
 
 
@@ -118,5 +174,4 @@ Route::get('/random-user', function()
 
 	return View::make('random-user')
 		->with('users', $users);
-
 });
--- app/views/lorem-ipsum.blade.php
+++ app/views/lorem-ipsum.blade.php
@@ -5,7 +5,7 @@
 @stop
 
 @section('head')
-  <link rel="stylesheet" href='css/lorem-ipsum.css' />
+  <link rel="stylesheet" href='{{ asset('css/lorem-ipsum.css') }}' />
 @stop
 
 @section('content')
@@ -13,15 +13,26 @@
   <h1>Lorem Ipsum Generator</h1>
 
 	<p>How many paragraphs do you want?</p>
-  <div;> 
+  <div class="form col-sm-6 col-sm-offset-4"> 
     {{ Form::open(array('url' => '/lorem-ipsum', 'method' => 'GET', 'name' => 'lorem-ipsum')) }}
       {{ Form::label('paragraphs', 'Paragraphs') }}
-      {{ Form::text('paragraphs') }}
+      {{ Form::text('paragraphs', 3) }}
+      <br>
+      {{ Form::label('font-size', 'Font Size') }}
+      {{ Form::select('font-size', array('8px' => '8 px', '10px' => '10 px', '12px' => '12 px', '14px' => '14 pt', '16px' => '16 px'), '16px') }}
+      <br>
+      {{ Form::label('font-face', 'Font Face') }}
+      {{ Form::select('font-face', array('roboto' => 'Roboto', 'helvetica' => 'Helvetica Neue', 'georgia' => 'Georgia', 'garamond' => 'Garamond' )) }}
+      <br>
+      {{ Form::label('columns', 'Columns') }}
+      {{ Form::select('columns', array(1 => 1, 2 => 2, 3 => 3)) }}
+      <br>
       {{ Form::submit('Generate!', array('class' => 'btn btn-default')) }}
+
     {{ Form::close() }}
   </div>
-  <div id="output" class="col-sm-8 col-sm-offset-2"> 
-    {{ $output }}
+  <div id="paragraphs" class="col-sm-10 col-sm-offset-1 {{ $fontSize }} {{ $fontFace }} {{ $columns }}"> 
+    {{ $paragraphs }}
   </div>
   <script type="text/javascript" language="JavaScript">
     document.forms['lorem-ipsum'].elements['paragraphs'].focus();
