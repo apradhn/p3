c0b4ecdd118ded5111f192367571da571a79bcbd
Abhijit Pradhan <abhijit.pradhan6@gmail.com>
2014-10-23 17:15:28 -0500

    annotated logic; removed unused code

--- app/routes.php
+++ app/routes.php
@@ -2,7 +2,8 @@
 /**
  * Returns random Lorem Ipsum text
  * 
- * @param string | int $p 
+ * @param string|int $p 
+ * @return Lorem Ipsum text
  */
 function generate_loremipsum($p) {
 	// checks for invalid user input
@@ -11,8 +12,10 @@ function generate_loremipsum($p) {
 		// create Lorem Ipsum generator 
 		$generator = new Badcow\LoremIpsum\Generator();
 
+		// generate $p number of lorem ipsum paragraphs
 		$paragraphs = $generator->getParagraphs($p);
 
+		// return lorem ipsum text 
 		return "<p>".implode('</p><p>', $paragraphs)."</p>";
 	}
 	// Checks if user entered a non-numeric string 
@@ -22,9 +25,10 @@ function generate_loremipsum($p) {
 }
 
 /**
- * Returns a font-size
+ * Returns a font-size as a css class
  * 
  * @param string $fs
+ * @return font-size class
  */ 
 function font_size($fs) {
 	if ($fs == 8) {
@@ -45,9 +49,10 @@ function font_size($fs) {
 }
 
 /**
- * Returns a column #
+ * Returns a column # in as a css class
  * 
- * c 
+ * @param int $c 
+ * @return columns class 
  */
 function columns($c) {
 	if ($c == 2) {
@@ -59,9 +64,10 @@ function columns($c) {
 }
 
 /** 
- * Returns a font face
+ * Returns a font face as a css class
  * 
  * @param string $ff
+ * @return font face
  */
 function font_face($ff) {
 	if ($ff == 'roboto') {
@@ -79,26 +85,14 @@ function font_face($ff) {
 }
 
 /**
- * Returns a the selected user inputs
- */
-function inputs() {
-	$paragraphs = Input::get('paragraphs');
-	$fontSize = Input::get('font-size');
-	$fontFace = Input::get('font-face');
-	$columns = Input::get('columns');
-
-	$inputs = "Paragraphs: ".$paragraphs." Font Size: ".$fontSize." Font Face: ".$fontFace." Column(s): ".$columns;
-
-	return $inputs; 
-}
-
-/**
  * Returns random user data
  *
  * @param string | int $u
+ * @return users 
  */
 function generate_users($u) {
 
+	// Determines the selected locale
 	if (Input::get('locale') == NULL) {
 		$locale = 'en_EN';
 	}
@@ -106,24 +100,25 @@ function generate_users($u) {
 	
 	// Creates a faker object 
 	$faker = Faker\Factory::create($locale);
+
+	// Collects user inputs
 	$users="";
 	$birthdate = Input::get('birthdate');
 	$profile = Input::get('profile');
-
 	$gender = Input::get('gender');
 
 	// Condition table for gender
 	if($gender == 'maleAndFemale') {
-		$alpha = true;
-	} else $alpha = false;
+		$both= true;
+	} else $both = false;
 
 	if($gender == 'female') {
-		$beta = true;
-	} else $beta = false;
+		$female = true;
+	} else $female = false;
 
 	if($gender == 'male') {
-		$gamma = true;
-	} else $gamma = false;
+		$male = true;
+	} else $male = false;
 
 	// Condtion table for birthdate & profile
 	if (((is_numeric($u)) or ($u == NULL)) and ($u < 100)) { // Executes of user input is valid 
@@ -148,7 +143,7 @@ function generate_users($u) {
 
 	if ($valid)  {
 		// executes of user input is numeric or null 
-		if ($alpha) {
+		if ($both) { // Executes if both genders selected 
 			if ($a) {
 				// Executes if neither birthdate nor profile are selected
 				for ($i=0; $i<$u; $i++) {
@@ -178,7 +173,7 @@ function generate_users($u) {
 				return $users;
 			}
 		}
-		elseif ($beta) {
+		elseif ($female) { // Executes if female gender selected 
 			if ($a) {
 				// Executes if neither birthdate nor profile are selected
 				for ($i=0; $i<$u; $i++) {
@@ -208,7 +203,7 @@ function generate_users($u) {
 				return $users;
 			}
 		}
-		elseif ($gamma) {
+		elseif ($male) { // Executes if male gender selected 
 			if ($a) {
 				// Executes if neither birthdate nor profile are selected
 				for ($i=0; $i<$u; $i++) {
@@ -241,21 +236,6 @@ function generate_users($u) {
 	} else return "<div class='error'>invalid input</div>";  // executes if user input is a non-numeric string
  } 
 
- /**
-  * Generates a random profile photo 
-  * 
-  * @param int $i
-  */
-/* function generate_image($i, $u) {
-
-		$faker = Faker\Factory::create();
-
-		$image = $faker->imageURL(200, 200, 'cats');
-
-		return "<img src=".$image.">";
-}
-*/
-
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -287,26 +267,22 @@ Route::get('/lorem-ipsum', function()
 	// Changes the number of columns of paragraphs
 	$columns = columns(Input::get('columns'));
 
-	// Displays selected inputs
-	$inputs = inputs();
-	
+	// Return lorem ipsum text to the browser 
 	return View::make('lorem-ipsum')
 		->with('paragraphs', $paragraphs)
 		->with('fontSize', $fontSize)
 		->with('fontFace', $fontFace)
-		->with('columns', $columns)
-		->with('inputs', $inputs);
+		->with('columns', $columns);
 });
 
 
 Route::get('/random-user', function() 
 {
-	// Retrieve number of users 
-	$u_count = Input::get('users');
 
 	// Generate random user data
-	$users = generate_users($u_count);
+	$users = generate_users(Input::get('users'));
 
+	// Return random users data to the browser
 	return View::make('random-user')
 		->with('users', $users);
 });
