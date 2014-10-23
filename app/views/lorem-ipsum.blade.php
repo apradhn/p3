8dedd3deecc26ba890c230d381abc1ce374a414e
Abhijit Pradhan <abhijit.pradhan6@gmail.com>
2014-10-22 20:21:33 -0500

    fixed styling that blocked the columns display; updated styles for form controls

--- app/views/lorem-ipsum.blade.php
+++ app/views/lorem-ipsum.blade.php
@@ -21,28 +21,36 @@
   <div class="container content">
     <div class="form col-sm-12">
       {{ Form::open(array('url' => '/lorem-ipsum', 'method' => 'GET', 'name' => 'lorem-ipsum')) }}
+        <div class="col-sm-2">
         {{ Form::label('paragraphs', 'Paragraphs') }}
         {{ Form::text('paragraphs', 3, array('size' => 5)) }}
-
+        </div>
+        <div class="col-sm-2">
         {{ Form::label('font-size', 'Font Size') }}
         {{ Form::select('font-size', array('8px' => '8 px', '10px' => '10 px', '12px' => '12 px', '14px' => '14 pt', '16px' => '16 px'), '16px') }}
-
+        </div>
+        <div class="col-sm-2">
         {{ Form::label('font-face', 'Font Face') }}
         {{ Form::select('font-face', array('roboto' => 'Roboto', 'helvetica' => 'Helvetica Neue', 'georgia' => 'Georgia', 'garamond' => 'Garamond' )) }}
-
+        </div>
+        <div class="col-sm-2">
         {{ Form::label('columns', 'Columns') }}
         {{ Form::select('columns', array(1 => 1, 2 => 2, 3 => 3)) }}
-
+        </div>
+        <div class="col-sm-2">
         {{ Form::submit('Generate!', array('class' => 'btn btn-default', 'id' => 'submit')) }}
-
+        </div>
       {{ Form::close() }}
     </div>
   </div>
 
-  
-  <div id="paragraphs" class="col-sm-10 col-sm-offset-1 {{ $fontSize }} {{ $fontFace }} {{ $columns }}"> 
-    {{ $paragraphs }}
-  </div>
+    <div class="row">
+      <div class="paragraphs {{ $fontSize }} {{ $fontFace }} {{ $columns }}"> 
+        {{ $paragraphs }}
+      </div>
+    </div>
+
+
 
   <script type="text/javascript" language="JavaScript">
     document.forms['lorem-ipsum'].elements['paragraphs'].focus();
